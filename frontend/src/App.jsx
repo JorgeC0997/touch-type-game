77a815e80ce3978471de456a9af1890e1fa94913
JorgeC0997 <jorge.constantinojr@outlook.com>
2024-12-01 14:03:40 -0800

    Cleared router scafolding

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,50 +1,11 @@
-import { useState } from "react";
 import "./App.css";
-import { Route, Routes, Outlet } from "react-router";
+import { Route, Routes } from "react-router";
+import Welcome from "./pages/Welcome";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  const Home = () => {
-    return (
-      <>
-        <h1>Home</h1>
-        <Outlet />
-      </>
-    );
-  };
-
-  const About = () => {
-    return (
-      <>
-        <h1>About</h1>
-      </>
-    );
-  };
-
-  const Dashboard = () => {
-    return (
-      <>
-        <h3>Dashboard</h3>
-      </>
-    );
-  };
-
-  const News = () => {
-    return (
-      <>
-        <h3>News</h3>
-      </>
-    );
-  };
-
   return (
     <Routes>
-      <Route path="/" element={<Home />}>
-        <Route index element={<Dashboard />} />
-        <Route path="News" element={<News />} />
-      </Route>
-      <Route path="/about" element={<About />} />
+      <Route path="/" element={<Welcome />} />
     </Routes>
   );
 }
