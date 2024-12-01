98682fd9d6ec04bb00f01f861e012dfed1a95848
JorgeC0997 <jorge.constantinojr@outlook.com>
2024-12-01 14:45:17 -0800

    Installed tailwindcss

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
-import "./App.css";
 import { Route, Routes } from "react-router";
 import Welcome from "./pages/Welcome";
+import NavBar from "./components/navbar/NavBar";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Welcome />} />
-    </Routes>
+    <>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Welcome />} />
+      </Routes>
+    </>
   );
 }
 
