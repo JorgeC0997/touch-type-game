9d9e3cb6c90713c83f19315d8bfb3bbf367290aa
JorgeC0997 <jorge.constantinojr@outlook.com>
2024-12-02 18:23:12 -0800

    Added theme colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        base: {
+          dark: "#222831",
+          neutral: "#393E46",
+          light: "#EEEEEE",
+        },
+        primary: "#D65A31",
+      },
+    },
   },
   plugins: [],
 };
