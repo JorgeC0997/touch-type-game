66864acd470a08580139af7503148c6e917cf660
JorgeC0997 <jorge.constantinojr@outlook.com>
2024-12-04 18:18:51 -0800

    Added Access control to header

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,6 +5,15 @@ import { SERVER_PORT } from "./config.js";
 const app = express();
 
 app.use(express.json());
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Access-Control-Allow-Headers"
+  );
+  next();
+});
 app.use(usersRoutes);
 
 app.use((req, res, next) => {
