2637eb063f370ddae86dcb8b00cd136816fea524
JorgeC0997 <jorge.constantinojr@outlook.com>
2024-12-11 16:57:17 -0800

    two variables added

--- backend/src/config.js
+++ backend/src/config.js
@@ -8,3 +8,6 @@ export const DB = process.env.DB || "touch_type";
 export const PASSWORD = process.env.PASSWORD || "&9zS1#YXb5NYp";
 export const DB_PORT = process.env.DB_PORT || 5432;
 export const SERVER_PORT = process.env.SERVER_PORT || 3001;
+export const JWT_SECRET =
+  process.env.JWT_SECRET || "782h463b53dd64088k34171afr784052";
+export const SALT_ROUNDS = process.env.SALT_ROUNDS || 10;
