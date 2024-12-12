954742a246ba929ef8eda2b163f770f33309b4b5
JorgeC0997 <jorge.constantinojr@outlook.com>
2024-12-11 16:58:26 -0800

    sql queires modified

--- backend/src/controllers/users.controllers.js
+++ backend/src/controllers/users.controllers.js
@@ -1,8 +1,11 @@
 import { pool } from "../dbConnection.js";
+import bcrypt from "bcrypt";
+import { SALT_ROUNDS } from "../config.js";
+import crypto from "crypto";
 
 export const getUsers = async (req, res) => {
   try {
-    const { rows } = await pool.query("SELECT * FROM users");
+    const { rows } = await pool.query("SELECT id, username FROM users");
     res.status(200).send(rows);
   } catch (error) {
     res.status(404).send({ message: error.message });
@@ -12,7 +15,10 @@ export const getUsers = async (req, res) => {
 export const getUser = async (req, res) => {
   const { id } = req.params;
   try {
-    const result = await pool.query("SELECT * FROM users WHERE id = $1", [id]);
+    const result = await pool.query(
+      "SELECT id, username FROM users WHERE id = $1",
+      [id]
+    );
     if (result.rowCount <= 0)
       return res.status(404).json({ message: "user not found" });
     res.status(200).send(result.rows[0]);
@@ -22,15 +28,21 @@ export const getUser = async (req, res) => {
 };
 
 export const createUser = async (req, res) => {
-  const { username, hash } = req.body;
+  const { username, password } = req.body;
+  const uuid = crypto.randomUUID();
   try {
-    const { rows } = await pool.query(
-      "INSERT INTO users (username, hash) VALUES ($1, $2) RETURNING *",
-      [username, hash]
-    );
-    res.status(200).send(rows[0]);
+    const hash = await bcrypt.hash(password, parseInt(SALT_ROUNDS));
+    try {
+      const { rows } = await pool.query(
+        "INSERT INTO users (id, username, hash) VALUES ($1, $2, $3) RETURNING *",
+        [uuid, username, hash]
+      );
+      res.status(200).send(rows[0]);
+    } catch (error) {
+      res.status(500).send({ message: error.message });
+    }
   } catch (error) {
-    res.status(500).send({ message: error.message });
+    res.status(400).send({ message: error.message });
   }
 };
 
@@ -48,14 +60,20 @@ export const deleteUser = async (req, res) => {
 
 export const updateUser = async (req, res) => {
   const { id } = req.params;
-  const { username, hash } = req.body;
+  const { username, password } = req.body;
   try {
-    const result = await pool.query(
-      "UPDATE users SET username = COALESCE($1, username), hash = COALESCE($2, hash) WHERE id = $3 RETURNING *",
-      [username, hash, id]
-    );
-    if (result.rowCount <= 0)
-      return res.status(404).json({ message: "user not found" });
-    res.status(200).send(result.rows[0]);
-  } catch (error) {}
+    const hash = await bcrypt.hash(password, parseInt(SALT_ROUNDS));
+
+    try {
+      const result = await pool.query(
+        "UPDATE users SET username = COALESCE($1, username), hash = COALESCE($2, hash) WHERE id = $3 RETURNING *",
+        [username, hash, id]
+      );
+      if (result.rowCount <= 0)
+        return res.status(404).json({ message: "user not found" });
+      res.status(200).send(result.rows[0]);
+    } catch (error) {}
+  } catch (error) {
+    res.status(400).send({ message: error.message });
+  }
 };
