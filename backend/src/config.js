80773b00628167e92939b4342366eed772f563a9
JorgeC0997 <jorge.constantinojr@outlook.com>
2024-12-29 14:46:39 -0800

    Created all global variables from .env file

--- backend/src/config.js
+++ backend/src/config.js
@@ -4,7 +4,7 @@ config();
 
 export const USER = process.env.USER || "postgres";
 export const HOST = process.env.HOST || "localhost";
-export const DB = process.env.DB || "touch_type";
+export const DB = process.env.DB || "postgres";
 export const PASSWORD = process.env.PASSWORD || "&9zS1#YXb5NYp";
 export const DB_PORT = process.env.DB_PORT || 5432;
 export const SERVER_PORT = process.env.SERVER_PORT || 3001;
