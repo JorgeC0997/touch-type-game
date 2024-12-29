a0e04c56cb17df9353e1448faaebe798c08bf5c4
JorgeC0997 <jorge.constantinojr@outlook.com>
2024-12-29 15:01:24 -0800

    Added all context providers to root

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,27 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router";
+import { AuthContextProvider } from "./context/AuthContext.jsx";
+import { ExerciseContextProvider } from "./context/ExerciseContext.jsx";
+import { AccountContextProvider } from "./context/AccountContext.jsx";
+import { ModalContextProvider } from "./context/ModalContext.jsx";
+import { ScoreContextProvider } from "./context/ScoreContext.jsx";
+import { UserContextProvider } from "./context/UserContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
+    <UserContextProvider>
+      <AuthContextProvider>
+        <AccountContextProvider>
+          <ScoreContextProvider>
+            <ExerciseContextProvider>
+              <ModalContextProvider>
+                <App />
+              </ModalContextProvider>
+            </ExerciseContextProvider>
+          </ScoreContextProvider>
+        </AccountContextProvider>
+      </AuthContextProvider>
+    </UserContextProvider>
   </BrowserRouter>
 );
