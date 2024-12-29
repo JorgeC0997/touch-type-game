eeb43e12e9d2b492ab8c0202eb76c2a2b3712cee
JorgeC0997 <jorge.constantinojr@outlook.com>
2024-12-29 14:59:15 -0800

    Added new routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,31 @@
+import { useContext } from "react";
 import { Route, Routes } from "react-router";
 import Welcome from "./pages/Welcome";
 import NavBar from "./components/navbar/NavBar";
+import Register from "./pages/Register";
+import PrivateRoutes from "./PrivateRoutes";
+import PracticeRoom from "./pages/PracticeRoom";
+import Home from "./pages/Home";
+import Modal from "./components/ui/Modal";
+import { ModalContext } from "./context/ModalContext";
 
 function App() {
+  const modalContext = useContext(ModalContext);
   return (
-    <>
+    <div className="relative">
       <NavBar />
+      {modalContext.isActive && <Modal>{modalContext.getContent()}</Modal>}
       <Routes>
-        <Route path="/" element={<Welcome />} />
+        <Route element={<PrivateRoutes />}>
+          <Route path="/">
+            <Route index element={<Home />} />
+            <Route path="room/:roomId" element={<PracticeRoom />} />
+          </Route>
+        </Route>
+        <Route path="/welcome" element={<Welcome />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
-    </>
+    </div>
   );
 }
 
