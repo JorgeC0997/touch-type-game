c4efda54bdc5a104f2bd0dbaa76f49dba2d27a78
JorgeC0997 <jorge.constantinojr@outlook.com>
2024-12-30 22:08:51 -0800

    New fonts added to project

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,15 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
         base: {
-          dark: "#222831",
-          neutral: "#393E46",
-          light: "#EEEEEE",
+          dark: '#222831',
+          neutral: '#393E46',
+          light: '#EEEEEE',
         },
-        primary: "#D65A31",
+        primary: '#D65A31',
+      },
+      fontFamily: {
+        pattaya: ['"Pattaya"'],
+        arOneSans: ['"AR One Sans"'],
+        righteous: ['"Righteous"'],
       },
     },
   },
