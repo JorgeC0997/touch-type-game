0feb9da0a0b7c1b7204d96ae6269f7251925fd15
JorgeC0997 <jorge.constantinojr@outlook.com>
2024-12-31 16:05:27 -0800

    Added forgot password form to login page

--- frontend/src/components/forms/LoginForm.jsx
+++ frontend/src/components/forms/LoginForm.jsx
@@ -1,17 +1,19 @@
-import React, { useState, useContext } from "react";
-import { AuthContext } from "../../context/AuthContext";
-import { useNavigate } from "react-router";
-import StyledInput from "../ui/StyledInput";
-import PrimaryButton from "../ui/PrimaryButton";
+import React, { useState, useContext } from 'react';
+import { AuthContext } from '../../context/AuthContext';
+import { useNavigate } from 'react-router';
+import StyledInput from '../ui/StyledInput';
+import PrimaryButton from '../ui/PrimaryButton';
+import ChangePasswordForm from './ChangePasswordForm';
 
 const LoginForm = () => {
   const authContext = useContext(AuthContext);
   const navigate = useNavigate();
-  const inputErrorStyles = "border-red-600";
-  const inputNormalStyles = "border-gray-500";
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  const inputErrorStyles = 'border-red-600';
+  const inputNormalStyles = 'border-gray-500';
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
   const [inputError, setInputError] = useState(false);
+  const [showPasswordForm, setShowPasswordFrom] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -19,20 +21,20 @@ const LoginForm = () => {
     if (!isUserLoggedin) {
       setInputError(true);
     } else {
-      setUsername("");
-      setPassword("");
+      setUsername('');
+      setPassword('');
       setInputError(false);
     }
   };
   return (
     <form
       onSubmit={handleSubmit}
-      method="post"
-      className="p-12 bg-base-neutral rounded-xl flex flex-col gap-y-4"
+      method='post'
+      className='p-12 bg-base-neutral rounded-xl flex flex-col gap-y-4'
     >
       <StyledInput
-        type="text"
-        placeholder="User Name"
+        type='text'
+        placeholder='User Name'
         errorState={inputError}
         onChange={(e) => {
           setUsername(e.target.value);
@@ -40,8 +42,8 @@ const LoginForm = () => {
         }}
       />
       <StyledInput
-        type="password"
-        placeholder="Password"
+        type='password'
+        placeholder='Password'
         errorState={inputError}
         onChange={(e) => {
           setPassword(e.target.value);
@@ -49,9 +51,23 @@ const LoginForm = () => {
         }}
       />
       {inputError && (
-        <p className="text-white text-sm">*Username and/or password is wrong</p>
+        <p className='text-white text-sm'>*Username and/or password is wrong</p>
       )}
-      <PrimaryButton legend="Log in" />
+
+      {showPasswordForm ? (
+        <ChangePasswordForm onCancel={() => setShowPasswordFrom(false)} />
+      ) : (
+        <div className='text-center'>
+          <button
+            type='button'
+            className='hover:underline hover:opacity-70'
+            onClick={() => setShowPasswordFrom(true)}
+          >
+            Forgot password?
+          </button>
+        </div>
+      )}
+      <PrimaryButton legend='Log in' />
     </form>
   );
 };
