d73670eeb37248876851f7e5b77b136219fd808d
JorgeC0997 <jorge.constantinojr@outlook.com>
2025-01-09 19:11:36 -0800

    Comments added to lib functions and variables

--- frontend/src/lib/Char.js
+++ frontend/src/lib/Char.js
@@ -42,18 +42,28 @@ export default class Char {
   };
 
   constructor(character, charState) {
+    // Character to be displayed in exercise text
     this.character = character;
+    // The state that will determine the style of the char
+    // it can be true, false or undefined
+    // PracticeRoom will set all Char objects to undefined based on an array of boolean characters
     this.charState = charState;
   }
 
+  // this will return the styles for Char object
   getStyle(currentPointer, charIndex) {
+    // if currentPointer and charIndex are equal it means that
+    // this char will be the active char to be typed by the user
     if (currentPointer == charIndex) {
       return this.active;
     } else if (this.charState == true) {
+      // return correct if char was typed correctly
       return this.correct;
     } else if (this.charState == false) {
+      // return incorrect if char was not typed correctly
       return this.incorrect;
     } else if (this.charState == undefined) {
+      // return defaultTxt if char is not ready to be typed
       return this.defaultTxt;
     }
   }
