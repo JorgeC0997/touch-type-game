9a446efe4dd40dd47536f637d3a496b893f80308
JorgeC0997 <jorge.constantinojr@outlook.com>
2025-01-09 18:34:13 -0800

    Deleted some imports and account context constant. Comments added

--- frontend/src/components/navbar/NavBar.jsx
+++ frontend/src/components/navbar/NavBar.jsx
@@ -1,30 +1,29 @@
-import React, { useContext } from 'react';
-import { Link } from 'react-router';
-import { AuthContext } from '../../context/AuthContext';
-import userLogo from '../../assets/user_logo.svg';
-import { ModalContext } from '../../context/ModalContext';
-import AccountInfo from '../ui/AccountInfo';
-import { AccountContext } from '../../context/AccountContext';
-import UserLogo from '../ui/UserLogo';
+import React, { useContext } from "react";
+import { Link } from "react-router";
+import { AuthContext } from "../../context/AuthContext";
+import { ModalContext } from "../../context/ModalContext";
+import AccountInfo from "../ui/AccountInfo";
+import UserLogo from "../ui/UserLogo";
 
 const NavBar = () => {
   const authContext = useContext(AuthContext);
-  const accountContext = useContext(AccountContext);
   const modalContext = useContext(ModalContext);
 
+  // This will activate the modal using AccountInfo component as the content of the modal
   const openAccountInfo = () => {
     modalContext.activateModal(() => {
       return <AccountInfo />;
     });
   };
+
   return (
-    <nav className='p-4 fixed top-0 left-0 w-full flex justify-between align-center bg-white text-black z-10'>
-      <Link to='/'>
-        <p className='my-auto h-auto text-lg font-righteous'>Touch Type _</p>
+    <nav className="p-4 fixed top-0 left-0 w-full flex justify-between align-center bg-white text-black z-10">
+      <Link to="/">
+        <p className="my-auto h-auto text-lg font-righteous">Touch Type _</p>
       </Link>
 
       {authContext.isUserAuth ? (
-        <ul className='flex gap-x-4'>
+        <ul className="flex gap-x-4">
           <button onClick={openAccountInfo}>
             <UserLogo />
           </button>
@@ -38,7 +37,7 @@ const NavBar = () => {
           </button>
         </ul>
       ) : (
-        <Link to='/register'>Register</Link>
+        <Link to="/register">Register</Link>
       )}
     </nav>
   );
